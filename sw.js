07bf52964af6fcd48d89d7725f1be34e744668c1
studiotnc7-cell <studiotnc7@gmail.com>
2026-08-07 23:47:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,26 @@
-const CACHE_NAME = 'tnc7-viz-cache-v1';
+const CACHE_NAME = 'tnc7-visualizer-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+    './index.html',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).catch(() => {
-            // Jika offline dan gagal mengambil aset, fallback ke sesuatu jika perlu
-            // (karena ini SPA/Satu file HTML, index.html harusnya sudah dicache)
-        });
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
