4f92f1798628e50f42fa13c1f76a4c2f1bdcf911
studiotnc7-cell <studiotnc7@gmail.com>
2026-08-15 00:56:35 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Ganti angka versi ini (misal: v3, v4, dst) SETIAP KALI Anda mengupdate index.html
-const CACHE_NAME = 'tnc7-visualizer-v14'; 
+const CACHE_NAME = 'tnc7-visualizer-v15'; 
 
 const urlsToCache = [
   './',
